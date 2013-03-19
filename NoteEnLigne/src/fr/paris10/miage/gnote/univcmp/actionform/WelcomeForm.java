25c5595922cebf24de5bed681275e2a66ca80230
saliha.belhocine <saliha.belhocine@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-03-19 22:52:44 +0000

    modification de WelcomeForm.jsp ajout du bean enseignant

--- NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/actionform/WelcomeForm.java
+++ NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/actionform/WelcomeForm.java
@@ -3,6 +3,22 @@ import org.apache.struts.action.ActionForm;
 
 
 public class WelcomeForm extends ActionForm {
+	private String login=null;
+	private String mdp=null;
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	
+	
 	
 
 }
