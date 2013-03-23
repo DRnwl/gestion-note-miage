0cdfddc3f2481a2141e9baebe45ec1e4b85b2329
saliha.belhocine <saliha.belhocine@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-03-23 18:51:50 +0000

    authentification marche!!!

--- trunk/NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/actions/LoginAction.java
+++ trunk/NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/actions/LoginAction.java
@@ -1,4 +1,5 @@
 package fr.paris10.miage.gnote.univcmp.actions;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +8,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import fr.paris10.miage.gnote.univcmp.actionform.LoginForm;
+import fr.paris10.miage.gnote.univcmp.basedonnees.QueryBD;
 
 
 
@@ -15,15 +17,28 @@ public class LoginAction extends Action  {
  public ActionForward execute( ActionMapping mapping,ActionForm form,
 		 HttpServletRequest request,HttpServletResponse response )throws Exception {
  
-	 LoginForm monLoginForm =(LoginForm) form;
-	 
-	
-	 // code metier interrogation base de données 
-	 
-	 if(monLoginForm.getLogin().equals("saliha")&&(monLoginForm.getMdp().equals("karima"))){
-		 return mapping.findForward("success");
-	 }
-	 return mapping.findForward("erreur");
- }
+	    LoginForm monLoginForm =(LoginForm) form;
+		ServletContext context = getServlet().getServletContext();
+		QueryBD conBase = new QueryBD(context.getRealPath(""));
+
+
+		if ((conBase.comparerIdentifiant(monLoginForm.getLogin(), monLoginForm.getMdp())).equals("enseignant")) {
+			return mapping.findForward("enseignant");
+		} else {
+			if ((conBase.comparerIdentifiant(monLoginForm.getLogin(), monLoginForm.getMdp())).equals("secretariat"))  {
+				return mapping.findForward("secretariat");
+			}else {
+				if((conBase.comparerIdentifiant(monLoginForm.getLogin(), monLoginForm.getMdp())).equals("etudiant")){
+					return mapping.findForward("etudiant");
+				}else{
+					return mapping.findForward("erreur");
+				}
+			}
+
+		}
+		
+	}
+		
+		
  }
 
