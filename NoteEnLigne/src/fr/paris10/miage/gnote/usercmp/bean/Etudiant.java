34cbe94a35b66964cdf7b7c1715bcc507e0cbe82
LydieTiago <LydieTiago@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-03-24 10:45:28 +0000

    Ajout d'une liste de RésultatExamen, des getters/setters associés et des fonctions suivantes: add+setResultatExamen et toString() qui permet d'afficher les notes de l'étudiant pour les examens passés

--- NoteEnLigne/src/fr/paris10/miage/gnote/usercmp/bean/Etudiant.java
+++ NoteEnLigne/src/fr/paris10/miage/gnote/usercmp/bean/Etudiant.java
@@ -1,6 +1,13 @@
 package fr.paris10.miage.gnote.usercmp.bean;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Date;
+
+
+import fr.paris10.miage.gnote.univcmp.bean.Examen;
 import fr.paris10.miage.gnote.univcmp.bean.Promotion;
+import fr.paris10.miage.gnote.univcmp.bean.ResultatExamen;
 
 /**
  * Classe représentant un étudiant. 
@@ -9,20 +16,22 @@ import fr.paris10.miage.gnote.univcmp.bean.Promotion;
  * @version 1.1
  *
  */
-public class Etudiant {
+public class Etudiant{
 
     /**
      * Clé primaire géré par la base et accessible uniquement en lecture.
      */
     private int numeroEtudiant;
     private static int nextEtudiant=0;
-    private int numeroCandidat;                                          
-    private String modeEvaluation;                                        
+    private Candidat candidat;                                          
+    private String modeEvaluation;   
+    private ArrayList<ResultatExamen> listResultatExamen;
 
     /**
      * Constructeur sans parametre.
      */
     public Etudiant() {
+    	
     }
 
     /**
@@ -31,36 +40,90 @@ public class Etudiant {
      * @param numeroEtudiant
      * @param promtion
      */
-    public Etudiant(int numeroCandidat, String modeEvaluation) {
-        this.numeroEtudiant=this.nextEtudiant++;
-    	this.numeroCandidat = numeroCandidat;
+    public Etudiant(Candidat candidat, String modeEvaluation) {
+    	this.listResultatExamen = new ArrayList<ResultatExamen>();
+    	this.numeroEtudiant=this.nextEtudiant++;
+    	this.candidat = candidat;
         this.modeEvaluation = modeEvaluation;
     }
 
     /***********************************************************************************************************************
-     ******************************************************LES ACCESSEURS******************************************************
+     ******************************************************LES GETTERS******************************************************
      ***********************************************************************************************************************/
     public int getNumeroEtudiant() {
         return numeroEtudiant;
     }
 
-    public int getNumeroCandidat() {
-        return this.numeroCandidat;
+    public Candidat getCandidat() {
+        return this.candidat;
     }
 
     public String getModeEvaluation() {
         return this.modeEvaluation;
     }
+    
+    
+    public ArrayList<ResultatExamen> getResultatExamen(){
+    	return this.listResultatExamen;
+    }
 
-    /***********************************************************************************************************************
+
+	/***********************************************************************************************************************
      ******************************************************SETTERS******************************************************
      ***********************************************************************************************************************/
     
-    public void setNumeroCandidat(int numeroCandidat) {
-        this.numeroCandidat = numeroCandidat;
+    public void setCandidat(Candidat candidat) {
+        this.candidat = candidat;
     }
 
     public void setModeEvaluation(String modeEvaluation) {
         this.modeEvaluation = modeEvaluation;
+    }
+
+    public void setListResultatExamen(ArrayList<ResultatExamen> listNote) {
+		this.listResultatExamen = listNote;
+	}
+
+/***********************************************************************************************************************
+ ******************************************************Add Note******************************************************
+ ***********************************************************************************************************************/
+    /**
+     * Ajouter une note a un Etudiant
+     * @param re : the grade
+     * @return true si la note a ete ajoutee
+     */
+    public boolean addResultatExamen(ResultatExamen re) {
+        return this.listResultatExamen.add(re);
+    }
+    
+    
+    /***********************************************************************************************************************
+     ******************************************************Set Note******************************************************
+     ***********************************************************************************************************************/
+   
+    public boolean setNote(Examen e, double note) {
+        boolean result = false;
+        for (ResultatExamen re : listResultatExamen) {
+            if (re.getExamen().getNumeroExamen() == e.getNumeroExamen()) {
+                re.setNote(note);
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+    
+ /***********************************************************************************************************************
+     ******************************************************Affichage******************************************************
+     ***********************************************************************************************************************/
+
+    @Override
+    public String toString() {
+	    String s = "<table><tr><td>Libelle Examen</td><td>Note</td></tr>";
+	    for (ResultatExamen re : listResultatExamen) {
+	        s = s + re.toString() ;
+	    }
+	    s = s + "</table>";
+	    return s;
     }
 }
