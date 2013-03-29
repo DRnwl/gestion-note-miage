605e1042eefe6badeee7f88775b640e08128b264
LydieTiago <LydieTiago@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-03-29 11:33:11 +0000

    Suppression du next_nformation et ajout d'un toString

--- NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/Formation.java
+++ NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/Formation.java
@@ -1,80 +1,104 @@
 package fr.paris10.miage.gnote.univcmp.bean;
 
+import fr.paris10.miage.gnote.usercmp.bean.Contrat_Quadrienal;
+
 
 public class Formation {
 
 	private int numeroFormation;
-	private static int nextFormation=0;
-	private int numeroContrat;
+	private Contrat_Quadrienal contrat;
 	private String libelle;
 	private String niveau;
 	private String type;
 	private String parcours;
 
+	/**
+     * Constructeur sans parametre.
+     */
 	public Formation() {
 	}
 
-
-
-
+	/**
+     * Constructeur avec parametres.
+     * @param numeroFormation
+     * @param contrat
+     * @param libelle
+     * @param niveau
+     * @param type
+     * @param parcours
+     */
+	public Formation(int numeroFormation, Contrat_Quadrienal contrat,
+			String libelle, String niveau, String type, String parcours) {
+		super();
+		this.numeroFormation = numeroFormation;
+		this.contrat = contrat;
+		this.libelle = libelle;
+		this.niveau = niveau;
+		this.type = type;
+		this.parcours = parcours;
+	}
+	
 	/***********************************************************************************************************************
-	 ****************************************************** LES GETTERS******************************************************
-	 ***********************************************************************************************************************/
+     ******************************************************LES GETTERS/SETTERS******************************************************/
+    
 
 	public int getNumeroFormation() {
 		return numeroFormation;
 	}
-	
-	
-	
-	public static int getNextFormation() {
-		return nextFormation;
+
+	public void setNumeroFormation(int numeroFormation) {
+		this.numeroFormation = numeroFormation;
 	}
 
-	public int getNumeroContrat() {
-		return numeroContrat;
+	public Contrat_Quadrienal getContrat() {
+		return contrat;
+	}
+
+	public void setContrat(Contrat_Quadrienal contrat) {
+		this.contrat = contrat;
 	}
 
 	public String getLibelle() {
 		return libelle;
 	}
 
-	public String getNiveau() {
-		return niveau;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public String getParcours() {
-		return parcours;
-	}
-
-	/***********************************************************************************************************************
-	 ****************************************************** LES SETTERS******************************************************
-	 ***********************************************************************************************************************/
-
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
+	}
+
+	public String getNiveau() {
+		return niveau;
 	}
 
 	public void setNiveau(String niveau) {
 		this.niveau = niveau;
 	}
 
+	public String getType() {
+		return type;
+	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
 
-	public void setParcours(String parcours) {
-		this.parcours = parcours;
+	public String getParcours() {
+		return parcours;
 	}
 
-	public void setNumeroContrat(int numeroContrat) {
-		this.numeroContrat = numeroContrat;
+	public void setParcours(String parcours) {
+		this.parcours = parcours;
+	}	
+	
+	/***********************************************************************************************************************
+	 ****************************************************** Affichage******************************************************
+	 ***********************************************************************************************************************/
+	public String toString(){
+		return "<tr><td>" + this.numeroFormation + "</td><td>" + this.contrat + "</td></tr>" 
+				+ "</td><td>" + this.libelle + "</td></tr>" 
+				+ "</td><td>" + this.niveau + "</td></tr>" 
+				+ "</td><td>"+ this.type + "</td></tr>" 
+				+ "</td><td>" + this.parcours + "</td></tr>";
 	}
-	
-	
 
 }
