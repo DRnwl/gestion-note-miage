fc31bc5181c9dacd63ea441672382ae92f89a26a
LydieTiago <LydieTiago@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-04-02 21:42:11 +0000

    ajout de system.out.println() dans la méthode affecterResultatExamen()

--- trunk/NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/basedonnees/QueryBD.java
+++ trunk/NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/basedonnees/QueryBD.java
@@ -10,9 +10,15 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+import fr.paris10.miage.gnote.univcmp.bean.EC;
+import fr.paris10.miage.gnote.univcmp.bean.Examen;
 import fr.paris10.miage.gnote.univcmp.bean.Formation;
 import fr.paris10.miage.gnote.univcmp.bean.Promotion;
 import fr.paris10.miage.gnote.univcmp.bean.PromotionEtudiant;
+import fr.paris10.miage.gnote.univcmp.bean.ResultatExamen;
+import fr.paris10.miage.gnote.univcmp.bean.Semestre;
+import fr.paris10.miage.gnote.univcmp.bean.TypeT;
+import fr.paris10.miage.gnote.univcmp.bean.UE;
 import fr.paris10.miage.gnote.usercmp.bean.Contrat_Quadrienal;
 import fr.paris10.miage.gnote.usercmp.bean.Etudiant;
 
@@ -138,6 +144,91 @@ public class QueryBD {
 
 	}
 	
+	public void affectResultatExamen(Etudiant st) throws SQLException{
+		System.out.println("je suis dans la fonction affectResultatExamen de l'étudiant n°"+st.getNumeroEtudiant());
+        try {
+            PreparedStatement pst = cx.prepareStatement("SELECT distinct a_passe.netudiant, a_passe.nexamen, " +
+            		"a_passe.note, examen.date_examen, examen.ntype, examen.nformation, examen.nue, " +
+            		"examen.nec, examen.horaire, examen.libelle as libelle_examen, examen.pourcentage, " +
+            		"formation.ncontrat, formation.libelle as libelle_formation, formation.niveau, " +
+            		"formation.type_formation, formation.parcours, contrat_quadriennal.date_contrat," +
+            		" contrat_quadriennal.duree, ue.nsemestre, ec.libelle as libelle_ec, ec.coef, " +
+            		"semestre.libelle as libelle_semestre, typet.libelle as libelle_type " +
+            		"FROM A_PASSE, EXAMEN,FORMATION,contrat_quadriennal, UE, EC, SEMESTRE, TYPET" +
+            		" Where A_PASSE.NEXAMEN=EXAMEN.NEXAMEN and EXAMEN.NFORMATION=FORMATION.NFORMATION " +
+            		"and FORMATION.NCONTRAT=CONTRAT_QUADRIENNAL.NCONTRAT and EXAMEN.NEC=EC.NEC " +
+            		"and EC.NUE=UE.NUE and ue.nsemestre=semestre.nsemestre and examen.ntype=typet.ntype" +
+            		" and A_PASSE.netudiant=?");
+            pst.setInt(1, st.getNumeroEtudiant());            
+            ResultSet rs = pst.executeQuery();
+                while (rs.next()) {
+                	Contrat_Quadrienal contrat= new Contrat_Quadrienal();
+                    Formation formation=new Formation();
+                    Semestre semestre=new Semestre();
+                    UE ue=new UE();
+                    EC ec=new EC();
+                    TypeT type=new TypeT();
+                    Examen examen = new Examen();
+                    ResultatExamen note= new ResultatExamen();
+                    
+                    contrat.setNumeroContrat(rs.getInt("NCONTRAT"));
+                    contrat.setDateContrat(rs.getDate("DATE_CONTRAT"));
+                    contrat.setDuree(rs.getFloat("DUREE"));
+                    System.out.println("contrat: "+contrat.getNumeroContrat());
+                    
+                    formation.setContrat(contrat);
+                    formation.setNumeroFormation(rs.getInt("NFORMATION"));
+                    formation.setLibelle(rs.getString("LIBELLE_FORMATION"));
+                    formation.setNiveau(rs.getString("NIVEAU"));
+                    formation.setType(rs.getString("TYPE_FORMATION"));
+                    formation.setParcours(rs.getString("PARCOURS"));
+                    System.out.println("formation: "+formation.getNumeroFormation());
+                    
+                    semestre.setNumeroSemestre(rs.getInt("NSEMESTRE"));
+                    semestre.setLibelle(rs.getString("libelle_semestre"));
+                    System.out.println("semestre: "+semestre.getNumeroSemestre());
+                    
+                    ue.setNumeroUE(rs.getInt("NUE"));
+                    ue.setFormation(formation);
+                    ue.setSemestre(semestre);
+                    System.out.println("UE: "+ue.getNumeroUE());
+                    
+                    ec.setNumeroEC(rs.getInt("NEC"));
+                    ec.setUe(ue);
+                    ec.setFormation(formation);
+                    ec.setLibelle(rs.getString("libelle_ec"));
+                    ec.setCoef(rs.getFloat("coef"));
+                    System.out.println("ec: "+ ec.getNumeroEC() + ", libelle: "+ec.getLibelle());
+                    
+                    type.setNumeroType(rs.getInt("NTYPE"));
+                    type.setLibelle(rs.getString("libelle_type"));
+                    System.out.println("type: "+type.getNumeroType()+ ", libelle: "+type.getLibelle()+ ", class: "+type.getClass());
+                    
+                    examen.setUe(ue);
+                    examen.setEc(ec);
+                    examen.setFormation(formation);
+                    examen.setDateExamen(rs.getDate("DATE_EXAMEN"));
+                    examen.setLibelle(rs.getString("libelle_examen"));
+                    examen.setHoraire(rs.getString("HORAIRE"));
+                    examen.setPoucentage(rs.getFloat("POURCENTAGE"));
+                    examen.setType(type);
+                    System.out.println("examen: "+examen.getNumeroExamen() +", libelle: "+examen.getLibelle());
+                    
+                    note.setEtudiant(st);
+                    note.setExamen(examen);
+                    note.setNote(rs.getDouble("NOTE"));
+                    System.out.println("note: "+ note.getNote());
+                    
+                    st.addResultatExamen(note);                    
+                }
+                rs.close();
+                pst.close();
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        System.out.println("je suis dans la fonction affectResultatExamen");
+    }
+
 	public void affectPromotionEtudiant(Etudiant st) throws SQLException{
         try {
         	System.out.println(cx);
@@ -174,7 +265,6 @@ public class QueryBD {
             e.getMessage();
         }
     }
-
 
 }
 
