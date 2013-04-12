98178c58547c4ff6d776e0acde25820646cf486a
LydieTiago <LydieTiago@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-04-12 14:45:34 +0000

    suppression des commentaires et méthodes inutiles

--- NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/UE.java
+++ NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/UE.java
@@ -8,7 +8,6 @@ public class UE {
 	private int numeroUE;
 	private Semestre semestre;
 	private Formation formation;
-	//private String libelle;
 
 	/**
 	 * Conctructeur sans parametre.
@@ -40,22 +39,9 @@ public class UE {
 		return formation;
 	}
 
-
-	/*public String getLibelle() {
-		return libelle;
-	}*/
-
-
-
 	/***********************************************************************************************************************
 	 ****************************************************** LES SETTERS******************************************************
 	 ***********************************************************************************************************************/
-
-	/*public void setLibelle(String libelle) {
-		this.libelle = libelle;
-	}*/
-
-
 	public void setNumeroUE(int numeroUE) {
 		this.numeroUE = numeroUE;
 	}
@@ -69,53 +55,6 @@ public class UE {
 	public void setFormation(Formation formation) {
 		this.formation = formation;
 	}
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((formation == null) ? 0 : formation.hashCode());
-		result = prime * result + numeroUE;
-		result = prime * result
-				+ ((semestre == null) ? 0 : semestre.hashCode());
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj){
-			return true;
-		}
-		if (obj == null){
-			return false;
-		}
-		if (getClass() != obj.getClass()){
-			return false;
-		}
-		UE other = (UE) obj;
-		if (formation == null) {
-			if (other.formation != null){
-				return false;
-			}
-		} else if (!formation.equals(other.formation)){
-			return false;
-		}
-		if (numeroUE != other.numeroUE){
-			return false;
-		}
-		if (semestre == null) {
-			if (other.semestre != null){
-				return false;
-			}
-		} else if (!semestre.equals(other.semestre)){
-			return false;
-		}
-		return true;
-	}
-
 
 	@Override
 	public String toString() {
