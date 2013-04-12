260f39af07e1068e34bc484095572d83c3e875e4
LydieTiago <LydieTiago@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-04-12 14:51:17 +0000

    suppression des méthodes hashcode et equals

--- NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/Promotion.java
+++ NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/Promotion.java
@@ -61,39 +61,4 @@ public class Promotion {
 		
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((formation == null) ? 0 : formation.hashCode());
-		result = prime * result + numeroPromotion;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj){
-			return true;
-		}
-		if (obj == null){
-			return false;
-		}
-		if (getClass() != obj.getClass()){
-			return false;
-		}
-		Promotion other = (Promotion) obj;
-		if (formation == null) {
-			if (other.formation != null){
-				return false;
-			}
-		} else if (!formation.equals(other.formation)){
-			return false;
-		}
-		if (numeroPromotion != other.numeroPromotion){
-			return false;
-		}
-		return true;
-	}
-	
 }
