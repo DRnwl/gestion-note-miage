0f5a28037ad6f6dfbad2d1c80452f4377cb6b09d
LydieTiago <LydieTiago@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-04-12 14:54:46 +0000

    suppression de hashcode et equals

--- NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/Semestre.java
+++ NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/bean/Semestre.java
@@ -44,34 +44,7 @@ public class Semestre {
 	}
 
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + numeroSemestre;
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj){
-			return true;
-		}
-		if (obj == null){
-			return false;
-		}
-		if (getClass() != obj.getClass()){
-			return false;
-		}
-		Semestre other = (Semestre) obj;
-		if (numeroSemestre != other.numeroSemestre){
-			return false;
-		}
-		return true;
-	}
-
-
+	
 	@Override
 	public String toString() {
 		return "Semestre [numeroSemestre=" + numeroSemestre + ", libelle="
