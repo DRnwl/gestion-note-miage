bf59c84c3c941e22e05c92c776891e754db6ab33
LydieTiago <LydieTiago@0c6c45b6-c191-9904-5846-bd0f11b4f79e>
2013-04-13 01:50:06 +0000

    Action de modification du coeff d'un EC

---
+++ NoteEnLigne/src/fr/paris10/miage/gnote/univcmp/actionform/ModifierCoefECForm.java
@@ -0,0 +1,23 @@
+package fr.paris10.miage.gnote.univcmp.actionform;
+import org.apache.struts.action.ActionForm;
+
+public class ModifierCoefECForm extends ActionForm{
+	private String coef=null;
+	private String libelle=null;
+
+	public String getCoef() {
+		return coef;
+	}
+
+	public void setChoix(String coef) {
+		this.coef = coef;
+	}
+	
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+}
